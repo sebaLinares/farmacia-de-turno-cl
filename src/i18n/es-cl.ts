5c922a7f8d6d1ad782dbbf25272dec2f92965615
Sebastian Linares <slinaresl@gmail.com>
2022-12-01 15:01:07 -0300

    meanwhile

--- src/i18n/es-cl.ts
+++ src/i18n/es-cl.ts
@@ -4,5 +4,12 @@ export const esCl = {
 		subtitle:
 			'Haz click en el botón 👇 y busca la farmacia más cercana a tu 🏠',
 		startButtonLabel: 'COMENZAR',
+    shiftDrugstoreButtonLabel: 'Farmacia de turno',
+    emergencyDrugstoreButtonLabel: 'Farmacia de urgencia',
+		a11y: {
+      companyName: 'Farmacia de turno Chile',
+      openSidemenu: 'Abrir menu lateral',
+      closeSidemenu: 'Cerrar menu lateral',
+    }
 	},
 }
